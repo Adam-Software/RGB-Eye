724b6601865e412e26b84656a59fc4f5d89dad3d
ADAM <36617611+ALGN86@users.noreply.github.com>
2023-06-07 13:39:24 +1000

    Update EyeTestClass.py

--- Controller/EyeTestClass.py
+++ Controller/EyeTestClass.py
@@ -1,17 +1,61 @@
 import numpy as np
 import cv2
+import smbus
+import time
+
+i2c_bus = smbus.SMBus(1)
+
+CONST = 170
+CLR_SCR_COMMAND = 0xFF          #очистить RGB матрицу
+OFF_LED_CLOCKWISE = 0xFE       #выключение светодиодов по часовой стрелке
+OFF_LED_ANTICLOCKWISE = 0xFD  #выключение светодиодов против часовой стрелке
+ON_LED_CLOCKWISE = 0xFC      #включение светодиодов по часовой стрелке
+ON_LED_ANTICLOCKWISE = 0xFB #включение светодиодов против часовой стрелке
+PLAY_ANIMATION = 0xFA      #проиграть анимацию зашитую в памяти контроллера RGB
+SET_ADDRESS = 0x0A        #установить адрес I2C
+ON_LED_NOW = 0xF9        #включить светодиод
+
+RED = 2
+GREEN = 3
+BLUE = 4
+
+STEP = 0
+TIME = 0
+ANIM = 0
 
 # Класс LED
 class LED:
-    def __init__(self, index):
-        self.index = index
+    def __init__(self, i2c_bus, index, CONST, ON_LED_CLOCKWISE, OFF_LED_CLOCKWISE, RED, GREEN, BLUE, STEP, TIME, ANIM):
         self.is_on = False  # Изначально светодиод выключен
+        
+        self.client = i2c_bus
+        
+        self.index = index
+        self.CONST = CONST
+        self.ON_LED_CLOCKWISE = ON_LED_CLOCKWISE
+        self.OFF_LED_CLOCKWISE = OFF_LED_CLOCKWISE
+        
+        self.RED = RED
+        self.GREEN = GREEN
+        self.BLUE = BLUE
+        
+        self.STEP = STEP
+        self.TIME = TIME
+        self.ANIM = ANIM
 
-    def turn_on(self):
+    def turn_on(self, , RED, command):
         self.is_on = True
+        
+        pak = [self.CONST, self.ON_LED_CLOCKWISE, self.RED, self.index, self.index, self.STEP, self.TIME, self.ANIM]
+        b.write_smbus(0x5f, 0, pak)
+        time.sleep(0.004)
 
     def turn_off(self):
         self.is_on = False
+        
+        pak = [170, self.OFF_LED_CLOCKWISE, self.RED, self.index, self.index, self.STEP, self.TIME, self.ANIM]
+        self.client.write_smbus(0x5f, 0, pak)
+        time.sleep(0.004)
 
 # Создание списка объектов LED
 led_list = [LED(i + 1) for i in range(80)]
